db1afc85c8203c7e16c59f2b595d6b6454361e71
puvvada nandini <puvvada.nandini@gmail.com>
2026-02-17 18:39:02 +0530

    roboshell

--- roboshop.sh
+++ roboshop.sh
@@ -19,3 +19,23 @@ do
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
